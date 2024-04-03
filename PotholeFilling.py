17a3c16aed964c37d0446c4e60546c56cfb7fc42
andytzou0820 <165874730+andytzou0820@users.noreply.github.com>
2024-04-03 14:35:51 +0800

    Add files via upload

---
+++ PotholeFilling.py
@@ -0,0 +1,65 @@
+"""
+File: PotholeFilling.py
+Name: Anderson
+
+--------------------------
+This program shows karel filling 3
+potholes. Students learn the concept of
+decomposition through the process.
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    """
+    TODO:
+    """
+
+    for i in range (3):
+        go_in()
+        put_99()
+        go_out()
+
+def go_in():
+    """
+    pre_condition: Karel is at the upper left of the pothole,facing East.
+    past_condition: Karel is in the pothole,facing south.
+    """
+    move()
+    turn_right()
+    move()
+
+
+def go_out():
+    """
+    pre_condition:Karel is in the pothole,facing south.
+    past_condition:Karel is at the upper left of the pothole,facing East.
+    """
+    turn_around()
+    move()
+    turn_right()
+    move()
+
+
+def turn_right():
+    for i in range(3):
+        turn_left()
+
+
+def turn_around():
+    for i in range(2):
+        turn_left()
+
+
+def put_99():
+    for i in range(99):
+        put_beeper()
+
+
+
+
+
+# ----- DO NOT EDIT CODE BELOW THIS LINE ----- #
+if __name__ == '__main__':
+    execute_karel_task(main)
